7797b65c8317e7f32cd52dab01a0a592a4d2d363
ShalomHasidim <67283774+ShalomHasidim@users.noreply.github.com>
2020-10-13 22:39:36 +0300

    13.10.20

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -1,18 +1,163 @@
 import React from "react";
-import L from 'leaflet';
-import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import L, { popup } from 'leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import leafGreen from '../assets/leaf-green.png';
 import leafRed from '../assets/leaf-red.png';
 import leafOrange from '../assets/leaf-orange.png';
 import leafShadow from '../assets/leaf-shadow.png';
-import {getMarkers}  from '../services/Markers.jsx';
+import markers1 from '../services/Markers.js'
+import markers2 from '../services/MarkersCenter.js';
+import school from '../assets/school.png';
+import Center from '../assets/Center.png';
 import '../css/popup.css';
 
+
+
 function MyMap() {
-    
-    const schools;
-    const matnasim;
-    const markers = getMarkers(); 
+
+  const position = [31.903410, 34.806831];
+  return (
+    <React.Fragment>
+
+      <Map className="map" center={position} zoom={14}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {
+          markers1.map((item, index) => {
+            return (<Marker key={index} position={[parseFloat(item.X), parseFloat(item.Y)]} icon={icon} >
+              <Popup className="popup">
+    <span className="popup">כתובת: {item.Address}</span><br/>
+    <span> בית ספר: {item.School}</span><br/>
+    <span>טלפון: {item.Phone}</span><br/>
+  </Popup>
+            </Marker>)
+          })
+        }
+
+        {
+          markers2.map((item, index) => {
+            return( <Marker key={index} position={[parseFloat(item.X), parseFloat(item.Y)]} icon={Ic} >
+              <Popup className="popup">
+    <span className="popup">כתובת: {item.Address}</span><br/>
+    <span> מרכז קהילתי: {item.CommunityCenterName}</span><br/>
+    <span>טלפון: {item.Phone}</span><br/>
+  </Popup>
+            </Marker>)
+          })
+        }
+      </Map>
+    </React.Fragment>
+  )
+
+}
+
+function PopUp(props){
+  return(
+    <Popup className="popup">
+    <span className="popup">כתובת: {props.Address}</span><br/>
+    <span> בית ספר: {props.School}</span><br/>
+    <span>טלפון: {props.Phone}</span><br/>
+  </Popup>
+  );
+}
+
+const icon = L.icon({
+  iconUrl: school,
+  //shadowUrl: leafShadow,
+  iconSize: [30, 60], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor: [-3, -76]
+});
+
+const Ic = L.icon({
+  iconUrl: Center,
+  //shadowUrl: leafShadow,
+  iconSize: [30, 60], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62],  // the same for the shadow
+  popupAnchor: [-3, -76]
+});
+
+export default MyMap;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function MyMap() {
+    const markers = getMarkers();
+    const MarkersCenter = getMarkersCenter();
     const position =[31.903410, 34.806831];
     return (
         <Map className= "map" center={position} zoom={14}>
@@ -22,29 +167,27 @@ function MyMap() {
         />
          {markers.map((marker, index) => {
           const icon = L.icon({
-            iconUrl: leafGreen,
-            shadowUrl: leafShadow,
-            iconSize:     [30, 60], // size of the icon
-            shadowSize:   [50, 64], // size of the shadow
-            iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+            iconUrl: school,
+            //shadowUrl: school,
+            iconSize:     [30, 30], // size of the icon
+            shadowSize:   [20, 25], // size of the shadow
+            iconAnchor:   [22, 30], // point of the icon which will correspond to marker's location
             shadowAnchor: [4, 62],  // the same for the shadow
-            popupAnchor:  [-3, -76]
+            popupAnchor:  [-10, -10]
           });
           return (
             <Marker key={index} position={[parseFloat(marker.X), parseFloat(marker.Y)]} icon ={ icon } >
-            {marker}
             <Popup className="popup">
               <span className="popup">כתובת: {marker.Address}</span><br/>
-              <span> בית ספר: {marker.school}</span><br/>
-              <span>טלפון: {marker.phone}</span><br/>
+              <span> בית ספר: {marker.School}</span><br/>
+              <span>טלפון: {marker.Phone}</span><br/>
             </Popup>
           </Marker>
-          );
+        );
         })}
-
         </Map>
     )
 
 }
 
-export default MyMap;
+export default MyMap;*/
---
+++ src/services/MarkersCenter.js
@@ -0,0 +1,114 @@
+const markers2 = [
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות רחובות ההולנדית",
+        "Address": "הר תבור 3, רחובות",
+        "X": "31.89175731",
+        "Y": "34.78237044"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות מופת",
+        "Address": "לוין אפשטיין 22, רחובות",
+        "X": "31.89738403",
+        "Y": "34.80849058"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות מופת",
+        "Address": "לוין אפשטיין 22, רחובות",
+        "X": "31.89738403",
+        "Y": "34.80849058"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות מופת",
+        "Address": "משה זכריה 6, רחובות",
+        "X": "31.88779532",
+        "Y": "34.81624983"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "הספריה המרכזית",
+        "Address": "גולדין 6, רחובות",
+        "X": "31.89358722",
+        "Y": "34.81259767"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "עירונוער רחובות",
+        "Address": "בנימין 4, רחובות",
+        "X": "31.89437496",
+        "Y": "34.81213272"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות שוויץ המדע",
+        "Phone": "89310700",
+        "Address": "סירני 52, רחובות",
+        "X": "31.90107676",
+        "Y": "34.82130665"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות שוויץ רחובות החדשה",
+        "Phone": "86366171",
+        "Address": "קרוננברג 1, רחובות",
+        "X": "31.87915592",
+        "Y": "34.8115331"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות אושיות",
+        "Phone": "89377702",
+        "Address": "החבורה 20, רחובות",
+        "X": "31.88252036",
+        "Y": "34.8200334"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות חשמונאים",
+        "Phone": "89493927",
+        "Address": "חשמונאים 1, רחובות",
+        "X": "31.88521043",
+        "Y": "34.80275127"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות קרית משה",
+        "Phone": "89312700",
+        "Address": "גבריאלוב 20, רחובות",
+        "X": "31.88743945",
+        "Y": "34.78595275"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות נווה יהודה",
+        "Phone": "89312700",
+        "Address": "אהרונוביץ 14, רחובות",
+        "X": "31.90164307",
+        "Y": "34.80090467"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות רחובות הצעירה",
+        "Phone": "89312700",
+        "Address": "הרב זבולון גרז 2, רחובות",
+        "X": "31.88269264",
+        "Y": "34.79035744"
+    },
+    {
+        "Flag": "FALSE",
+        "CommunityCenterName": "חוויות כפר גבירול",
+        "Phone": "89472410",
+        "Address": "צאלון 1, רחובות",
+        "X": "31.89652162",
+        "Y": "34.77478686"
+    }
+
+]
+
+export default markers2;
+//export function getMarkersCenter() {
+  //  return MarkersCenter;
+//}
